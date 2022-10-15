a533f1c3516755be08dd6a7c926a0a1990fb0323
Jirka <jirka.borovec@seznam.cz>
2022-10-15 21:14:57 +0200

    loading requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+from pkg_resources import parse_requirements
 from setuptools import setup, find_packages
+
+
+def _load_requirements(file_path: str = "requirements.txt") -> list:
+    with open(file_path) as fp:
+        requires = parse_requirements(fp.readlines())
+    return list(requires)
 
 setup(
     name='latent-diffusion',
     version='0.0.1',
     description='',
     packages=find_packages(),
-    install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
+    install_requires=_load_requirements(),
 )
